d781915fd023ca5280028cfad83c3eb1c87a7dc4
Alex <alexyakimenkojr@gmail.com>
2025-03-05 12:26:04 +0200

    feature/sdl: add sdl with glad

--- main.c
+++ main.c
@@ -1,18 +1,72 @@
-#include <stdio.h>
-#include <string.h>
+#include <glad/glad.h>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
-#include "engine/io.h"
+static SDL_Window *window = nullptr;
+static SDL_GLContext context = nullptr;
 
-int main(void) {
-    const char* str = "Hello World!";
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char* argv[])
+{
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 
-    io_file_write("text.txt", str, strlen(str));
-
-    const File file = io_file_read("text.txt");
-
-    if (file.is_valid) {
-        puts(file.data);
+    if (SDL_Init(SDL_INIT_VIDEO) == false)
+    {
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Couldn't initialize SDL!", SDL_GetError(), nullptr);
+        return SDL_APP_FAILURE;
     }
 
-    return 0;
+    window = SDL_CreateWindow(
+            "2D GameEngine",
+            (int)1280,
+            (int)720,
+            SDL_WINDOW_OPENGL
+        );
+
+    if (window == NULL) {
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Couldn't create window/renderer!", SDL_GetError(), nullptr);
+        return SDL_APP_FAILURE;
+    }
+
+    context = SDL_GL_CreateContext(window);
+
+    if (!gladLoadGLLoader(SDL_GL_GetProcAddress)) {
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Couldn't initialize OpenGL!", SDL_GetError(), nullptr);
+        return SDL_APP_FAILURE;
+    }
+
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent(void *appstate, const SDL_Event *event)
+{
+    switch (event->type)
+    {
+        case SDL_EVENT_QUIT:
+                return SDL_APP_SUCCESS;
+        case SDL_EVENT_KEY_DOWN:
+            if (event->key.key == SDLK_ESCAPE)
+            {
+                return SDL_APP_SUCCESS;
+            }
+        default:
+            break;
+    }
+
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    SDL_GL_SwapWindow(window);
+    return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void *appstate, SDL_AppResult result)
+{
+    SDL_GL_DestroyContext(context);
 }
